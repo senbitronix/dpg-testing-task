0f8fce4ad1937adb9c9a480c7a1440f8ebc4eb34
Konstantin <k.chernov@pamika.ru>
2025-02-07 15:22:51 +0300

    testing task ready

---
+++ src/lib/components/shared/Notification.svelte
@@ -0,0 +1,18 @@
+<h1 class="notification">
+	<slot />
+</h1>
+
+<style lang="scss">
+	.notification {
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    background-color: rgba(105, 162, 236, 0.9);
+    color: white;
+    padding: 10px 15px;
+    border-radius: 5px;
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.5);
+    z-index: 1000;
+		font-size: 16px;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,6 +11,31 @@
 	import Feed from '$lib/components/feed/Feed.svelte';
 	import Loader from '$lib/components/shared/Loader.svelte';
 	import ErrorComponent from '$lib/components/shared/Error.svelte';
+	import NotificationComponent from '$lib/components/shared/Notification.svelte';
+
+
+	let loadingFromCache = false;
+
+	/**
+	 * Load From Cache
+	 * @description Загружаем посты из кеша
+	 */
+	const loadFromCache = () => {
+    const cachedPosts = localStorage.getItem('posts');
+    return cachedPosts ? JSON.parse(cachedPosts) : [];
+  };
+
+	/**
+	 * Show Notificatin
+	 * @description Показываем уведомление
+	 * @param {number} value - Current value
+	 */
+	const showNotification = (ms = 3000) => {
+    loadingFromCache = true;	
+		setTimeout(()=> {
+			loadingFromCache = false
+		}, ms)
+  };
 
 	/**
 	 * Load data
@@ -20,8 +45,20 @@
 		const response = await PostsApi.getAll();
 
 		if (!response.success) {
-			throw new Error(getReasonPhrase(response.data.code));
-		}
+			if (response.data.isNetworkError) {
+				const cachedPosts = loadFromCache();
+				if (cachedPosts.length) {					
+					showNotification()
+					return cachedPosts
+				} else {
+					return []
+				}
+			} else {
+				throw new Error(getReasonPhrase(response.data.code));
+			}
+		}		
+
+		localStorage.setItem('posts', JSON.stringify(response.data));
 
 		return response.data;
 	};
@@ -31,7 +68,17 @@
 {#await loadData()}
 	<Loader />
 {:then posts}
+	{#if loadingFromCache}
+		<NotificationComponent>
+			Загрузка данных из кеша
+		</NotificationComponent>
+  {/if}
 	<Feed {posts} />
+	{#if posts.length > 0}
+    <Feed {posts} />
+  {:else}
+    <p>Нет данных для отображения.</p>
+  {/if}
 {:catch e}
 	<ErrorComponent>
 		{e}
